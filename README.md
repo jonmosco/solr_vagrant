31e78c9355d24cce01b3b8a2e05d4c847e5687e7
Jon Mosco <jonny.mosco@gmail.com>
2014-09-15 20:06:58 -0400

    variable for solr version.  Updated README.md

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 #Solr Vagrant Environment
+
+This is a single node solr instance for testing/learning Apache Solr
+
+##Usage
+
+Requires Puppet Staging Module
+
+Checkout the submodules:
+
+```
+  $ git submodule init
+  $ git submodule update
+```
+
+  Boot the environment:
+  $ vagrant up --provider=vmware_fusion
--- manifests/default.pp
+++ manifests/default.pp
@@ -1,18 +1,26 @@
-## Puppet Manifests for setting up a proper Solr Environment
+## Puppet Manifest for setting up a proper Solr Environment
+
+## Version of Solr to install
+$solr_version = '4.10.0'
+
+service { 'iptables':
+  ensure => stopped,
+}
 
 package { 'java-1.7.0-openjdk':
   ensure => installed
 }
 
-staging::deploy { 'solr-4.10.0.tgz':
-    source  => 'http://apache.mirrors.hoobly.com/lucene/solr/4.10.0/solr-4.10.0.tgz',
+staging::deploy { "solr-${solr_version}.tgz":
+    source  => "http://apache.mirrors.hoobly.com/lucene/solr/${solr_version}/solr-${solr_version}.tgz",
     target  => '/usr/local/src',
     require => Package['java-1.7.0-openjdk'],
 }
 
 exec { 'start_solr':
-  command => '/usr/bin/java -jar start.jar',
-  cwd     => '/usr/local/src/solr-4.10.0/example',
+  command => "/usr/bin/java -jar /usr/local/src/solr-${solr_version}/example/start.jar",
+  cwd     => "/usr/local/src/solr-${solr_version}/example",
   onlyif  => '/bin/ps -ef | grep java',
-  require => Staging::Deploy['solr-4.10.0.tgz'],
+  timeout => 0,
+  require => Staging::Deploy["solr-${solr_verson}.tgz"],
 }
